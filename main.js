e14c79c1e5a40b394b1b8df46ec0f95c5c04b3b8
smashleycave <adcave85@gmail.com>
2020-07-13 18:51:44 -0400

    added form checks and submit button Javascript

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
+/* Toggle between adding and removing the "responsive" class to topnav*/
+function thisFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
       x.className += " responsive";
@@ -14,11 +14,11 @@ var slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
-
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
+// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -27,15 +27,20 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
+  if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  }
-      for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
+      slides[i].style.display = "none";
   }
-  
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
 
+/*--SUBMIT BUTTON-Thanks user for their submission--*/
+
+function myFunction() {
+    alert("Thank you for you submission! We will be in contact soon!");
+  }
